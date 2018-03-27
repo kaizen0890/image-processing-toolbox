14b386f0629e41ab445e28a4a0b636b4ad12a193
Weitai Chen <32036360+weitaichen@users.noreply.github.com>
2018-03-27 10:53:37 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,23 +16,25 @@ cv::Mat SPC(cv::Mat temp, float para1);
 int _tmain(int argc, char** argv)
 {
 	Mat input,output,colorimg;
-	if(argc < 3){
+	if(argc < 3){  //command exampl: main.exe, filename, method, parameter1, parameter2,..... 
 		cout<<"command invalid";
 		return -1;
 	}
 	string filename=argv[1];
 	filename=filename+".png";
-	int operation=stoi(string(argv[2]));
-	float para1=0;
+	int Method=stoi(string(argv[2])); //convert string to int
+	float para1=0,para2=0;
 	if(argc>3)
-		para1=stof(string(argv[3]));
-	input = imread(filename, 0); 
-	colorimg = imread(filename, 1); 
+		para1=stof(string(argv[3])); //convert string to float
+	if(argc>4)
+		para2=stof(string(argv[4])); //convert string to float
+	input = imread(filename, 0);  //read a gray image
+	colorimg = imread(filename, 1);  //read a color image
 	if(input.empty()){
 		cout<<("Can not load image: "+filename);
 		return -1;
 	}
-	switch( operation) {
+	switch( Method) {
 	case 1 :cout<<("show gray image:"+filename);
 			input.copyTo(output);
 			cv::imshow("gray image",output);
@@ -63,7 +65,7 @@ int _tmain(int argc, char** argv)
 			return -1;
 			break;
 	}
-	cv::imwrite("output.png",output);
+	cv::imwrite("output.png",output); //write an image in main.exe folder
 	waitKey();
 	return 0;
 }
@@ -71,25 +73,25 @@ int _tmain(int argc, char** argv)
 
 
 cv::Mat otsu(cv::Mat temp, int method){
-    cv::Mat otsu,RC;
-    temp.convertTo(temp,CV_8U);
+    cv::Mat otsu,RC; //RC is for RC Otsu's threshold method
+    temp.convertTo(temp,CV_8U); //image format change to unsigned 8bit/pixel 
 	temp.copyTo(otsu);
 	temp.copyTo(RC);
-    cv::threshold(temp,RC,140,255,CV_THRESH_OTSU);
+    cv::threshold(temp,RC,140,255,CV_THRESH_OTSU); //Otsu's theshold using OpenCV function(Only for RC Otsu's method)
     float pi=0,ui=0,u=0;
     float n=0;
-    vector <float> p0;
-    vector <float> p1;
-    vector <float> u0;
-    vector <float> u1;
-    vector <float> delta;
+    vector <float> p0; //p0 is cumulated probability for gray level less than threshold t
+    vector <float> p1; //p1 is cumulated probability for gray level greater than a threshold t
+    vector <float> u0; //u0 is mean gray level for pixels with gray level less than threshold t
+    vector <float> u1; //u1 is mean gray level for pixels with gray level greater than threshold t
+    vector <float> delta; //Delta is between-class variance
     float h[256];
     for(int k=0;k<256;k++)
         h[k]=0;
-    for(int i=0;i<otsu.rows;i++)
+    for(int i=0;i<otsu.rows;i++) //compute histogram of input image
         for(int j=0;j<otsu.cols;j++){
             if(method==3){
-				if(RC.ptr<uchar>(i)[j]==0){
+				if(RC.ptr<uchar>(i)[j]==0){ //RC Otsu's method only count pixel with gray level greater than first Otsu's threshold
 					h[otsu.ptr<uchar>(i)[j]]++;
 					n++;
 				}
@@ -121,7 +123,7 @@ cv::Mat otsu(cv::Mat temp, int method){
         u=0;
         ui=0;
     }
-	if(method==0 || method==3) //Otsu's threshold
+	if(method==0 || method==3) //Otsu's threshold && RC Otsu's method
 		for(int k=0;k<256;k++)
 		 delta.push_back( p0[k]*u0[k]*u0[k]+p1[k]*u1[k]*u1[k] );
 	else if(method==1) //VE method
@@ -132,25 +134,28 @@ cv::Mat otsu(cv::Mat temp, int method){
 		 delta.push_back(  p0[k]*p0[k]*u0[k]*u0[k]+p1[k]*u1[k]*u1[k] );
     float max=0;
     int TH=0;
-    for(int i=0;i<256;i++)
+    for(int i=0;i<256;i++) //find threshold TH with maximum between-class variance
         if(delta[i]>max){
             max = delta[i];
             TH=i;
         }
-	cv::threshold(temp,otsu,TH,255,CV_THRESH_BINARY);
+	cv::threshold(temp,otsu,TH,255,CV_THRESH_BINARY); //Binary threshold with computed threshold TH
+	cout<<"Computed threshold value is:"<<TH;
     return otsu;
 }
 cv::Mat SPC(cv::Mat temp, float para1){
 	cv::Mat SPC;
 	temp.copyTo(SPC);
     Scalar mean,std;
-    cv::meanStdDev(SPC,mean,std);
+    cv::meanStdDev(SPC,mean,std); //find mean and standard deviation of gray level in input image
     float c=para1;
     for(int i=0;i<SPC.rows;i++)
         for(int j=0;j<SPC.cols;j++)
-            if(SPC.ptr<uchar>(i)[j]>mean.val[0]-c*std.val[0] && SPC.ptr<uchar>(i)[j]<mean.val[0]+c*std.val[0])
+            if(SPC.ptr<uchar>(i)[j]>mean.val[0]-c*std.val[0] && SPC.ptr<uchar>(i)[j]<mean.val[0]+c*std.val[0]) //threshold out pixel with gray level less and greater than c*standard deviation from mean
                 SPC.ptr<uchar>(i)[j]=0;
             else
                 SPC.ptr<uchar>(i)[j]=255;
+	cout<<"Computed lower bound value is:"<<mean.val[0]-c*std.val[0]<<endl;
+	cout<<"Computed upper bound value is:"<<mean.val[0]+c*std.val[0];
     return SPC;
 }
